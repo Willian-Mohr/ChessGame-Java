288147d6619c7b31993637c6b911edf7b3b19df4
Willian-Mohr <willianbmohr@gmail.com>
2021-04-27 22:38:28 -0300

    Implementing possible moves of Rook

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -2,33 +2,38 @@ package boardgame;
 
 public class Position {
 
-	private Integer row;
-	private Integer column;
+    private Integer row;
+    private Integer column;
 
-	public Position(Integer row, Integer column) {
-		this.row = row;
-		this.column = column;
-	}
+    public Position(Integer row, Integer column) {
+        this.row = row;
+        this.column = column;
+    }
 
-	public Integer getRow() {
-		return row;
-	}
+    public Integer getRow() {
+        return row;
+    }
 
-	public void setRow(Integer row) {
-		this.row = row;
-	}
+    public void setRow(Integer row) {
+        this.row = row;
+    }
 
-	public Integer getColumn() {
-		return column;
-	}
+    public Integer getColumn() {
+        return column;
+    }
 
-	public void setColumn(Integer column) {
-		this.column = column;
-	}
+    public void setColumn(Integer column) {
+        this.column = column;
+    }
 
-	@Override
-	public String toString() {
-		return row + ", " + column;
-	}
+    @Override
+    public String toString() {
+        return row + ", " + column;
+    }
+
+    public void setValues(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
 
 }
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -2,18 +2,24 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Piece;
+import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
 
-	private chess.Color color;
+    private chess.Color color;
 
-	public ChessPiece(Board board, chess.Color color) {
-		super(board);
-		this.color = color;
-	}
+    public ChessPiece(Board board, chess.Color color) {
+        super(board);
+        this.color = color;
+    }
 
-	public chess.Color getColor() {
-		return color;
-	}
+    public chess.Color getColor() {
+        return color;
+    }
+
+    protected boolean isThereOpponentPiece(Position position) {
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+        return p != null && p.getColor() != color;
+    }
 
 }
