fd8bd3f69593259421e66b4f4dc2a759c9486554
Willian-Mohr <willianbmohr@gmail.com>
2021-05-01 22:10:55 -0300

    Ignore Eclipse folder

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -30,7 +30,7 @@ public class Pawn extends ChessPiece {
     public boolean[][] possibleMoves() {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
         Position p = new Position(0, 0);
-        
+
         if (getColor() == Color.WHITE) {
             p.setValues(position.getRow() - 1, position.getColumn());
             if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
